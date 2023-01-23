90dd699d9dbae3150672a537368832d5d5ae60a6
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-01-23 14:34:08 +0000

    Updated JS file
    With this update I added a function which works to randomise the selection on a number of Objects. This function will 'output' an inspiring quote which is one of the properties of the object.

--- index.js
+++ index.js
@@ -2,39 +2,56 @@ const quotes = [
   {
     quote: "The shimmering stars danced in the midnight sky.",
     author: "- Coby Jones",
-    image: "images/img-1.jpg",
+    image: "/images/img-1.jpg",
   },
 
   {
     quote: "The quick brown fox jumps over the lazy dog.",
     author: "- Harry Smith ",
-    image: "images/img-2.jpg",
+    image: "/images/img-2.jpg",
   },
 
   {
     quote: "Golden leaves rustled in the autumn breeze.",
     author: "- Sally Boop",
-    image: "images/img-3.jpg",
+    image: "/images/img-3.jpg",
   },
 
   {
     quote: "The ocean waves whispered secrets to the shore.",
     author: "- Anne Jilly",
-    image: "images/img-4.jpg",
+    image: "/images/img-4.jpg",
   },
 
   {
     quote: "A symphony of colors painted the sunset sky.",
     author: "- Howard Burn",
-    image: "images/img-5.jpg",
+    image: "/images/img-5.jpg",
   },
 ];
 
 const wrapper = document.getElementById("wrapper");
-const container = document.getElementById("container");
 const quoteBox = document.getElementById("quote-box");
 let quoteOutput = document.getElementById("quote-output");
 let authorOutput = document.getElementById("author-output");
 
-quoteOutput.textContent = quotes[0].quote;
-authorOutput.textContent = quotes[0].author;
+// function that randomly changes quote/author & background image
+
+// randomise object
+function randomiseObject() {
+  let randomObject = Math.floor(Math.random() * quotes.length);
+  return quotes[randomObject];
+}
+
+let obj = randomiseObject();
+
+// print properties on page
+
+console.log(obj.quote);
+console.log(obj.author);
+console.log(obj.image);
+
+quoteOutput.textContent = obj.quote;
+authorOutput.textContent = obj.author;
+
+document.querySelector(".container").style.backgroundImage = obj.image;
